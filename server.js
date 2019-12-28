376e5fc4a91c23c846f02487c405b9f33de456ab
Okafor Ikenna <ikenna@kudabank.com>
2019-12-28 22:28:30 +0100

    Fixing heroku deploy

--- server.js
+++ server.js
@@ -3,11 +3,11 @@ const path = require('path');
 const app = express();
 
 // Serve static files....
-app.use(express.static(__dirname + '/dist/kudaAgency/'));
+app.use(express.static(__dirname + '/dist/testapp/'));
 
 // Send all requests to index.html
 app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/kudaAgency/index.html'));
+  res.sendFile(path.join(__dirname + '/dist/testapp/index.html'));
 
 
 });
