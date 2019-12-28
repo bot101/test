7e079c4ebc334f3305d036172e50e37f6d50e0d5
Okafor Ikenna <ikenna@kudabank.com>
2019-12-28 22:33:49 +0100

    Fixing deploy

--- server.js
+++ server.js
@@ -3,11 +3,11 @@ const path = require('path');
 const app = express();
 
 // Serve static files....
-app.use(express.static(__dirname + '/dist/testapp/'));
+app.use(express.static(__dirname + '/dist/testApp/'));
 
 // Send all requests to index.html
 app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/testapp/index.html'));
+  res.sendFile(path.join(__dirname + '/dist/testApp/index.html'));
 
 
 });
